4f8761035c6e20fe20c876721c2c61c781822a52
Kay Schecker <sayhello@kay-schecker.de>
2020-09-21 22:59:04 +0200

    added json schema

--- apps/generator-cli/src/app/services/config.service.ts
+++ apps/generator-cli/src/app/services/config.service.ts
@@ -4,17 +4,18 @@ import {LOGGER} from '../constants';
 import {set, get} from 'lodash';
 import * as fs from 'fs-extra';
 
-interface ConfigInterface {
-  spaces: number
-  'generator-cli': {
-    version: string
-  }
-}
-
 @Injectable()
 export class ConfigService {
 
   public readonly configFile = path.resolve(process.env.INIT_CWD || process.cwd(), 'openapitools.json')
+
+  private readonly defaultConfig = {
+    $schema: 'node_modules/@openapitools/openapi-generator-cli/config.schema.json',
+    spaces: 2,
+    'generator-cli': {
+      version: undefined,
+    },
+  }
 
   constructor(
     @Inject(LOGGER) private readonly logger: LOGGER,
@@ -32,10 +33,10 @@ export class ConfigService {
 
   private read() {
     fs.ensureFileSync(this.configFile)
-    return fs.readJSONSync(this.configFile, {throws: false, encoding: 'utf8'}) || {}
+    return fs.readJSONSync(this.configFile, {throws: false, encoding: 'utf8'}) || this.defaultConfig
   }
 
-  private write(config: ConfigInterface) {
+  private write(config) {
     fs.ensureFileSync(this.configFile)
     fs.writeJSONSync(this.configFile, config, {encoding: 'utf8', spaces: config.spaces || 2})
   }
