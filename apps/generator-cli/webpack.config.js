ce67ddcfc9eb979115de959ee39cfd43314b17ad
Kay Schecker <sayhello@kay-schecker.de>
2020-09-22 20:42:14 +0200

    increased version

--- apps/generator-cli/webpack.config.js
+++ apps/generator-cli/webpack.config.js
@@ -5,7 +5,7 @@ const {BannerPlugin} = require('webpack')
 module.exports = (config) => {
   const basePackageValues = {
     ...packageConfig,
-    version: `${version}-beta8`,
+    version: `${version}-beta9`,
     name: `@${name}/openapi-generator-cli`,
     description: 'A npm package wrapper for OpenAPI Generator (https://github.com/OpenAPITools/openapi-generator), generates which API client libraries (SDK generation), server stubs, documentation and configuration automatically given an OpenAPI Spec (v2, v3)',
     main: "./main.js",
