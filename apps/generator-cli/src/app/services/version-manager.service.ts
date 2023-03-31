9e6f443d38ee9affdf7eb47ed89e8919756b7991
Michael Bahr <michael@bahr.dev>
2023-03-31 14:26:52 +0200

    chore(deps): upgrade @nestjs to 9.3.9 (#693)
    * chore(deps): upgrade @nestjs to 9.3.9
    
    * chore(deps): expect error TS2339
    
    * chore(deps): update dependency @types/lodash to v4.14.191
    
    * chore(deps): update dependency dotenv to v16.0.3
    
    * chore(deps): update dependency ts-jest to v27.1.5
    
    * fix(deps): update dependency compare-versions to v4.1.4
    
    * fix(deps): update dependency inquirer to v8.2.5
    
    * fix(deps): update nest monorepo
    
    * chore(deps): update commitlint monorepo
    
    * chore(deps): update dependency @types/jest to v27.5.2
    
    * chore(deps): update dependency @types/node to v16.18.18
    
    * chore(deps): update dependency eslint-config-prettier to v8.8.0
    
    * fix(deps): update nest monorepo to v9.3.11
    
    * chore(deps): update dependency prettier to v2.8.6
    
    * chore(deps): update dependency ts-node to v10.9.1
    
    * chore(deps): update dependency type-fest to v2.19.0
    
    * chore(deps): update dependency typescript to v4.9.5
    
    * chore(deps): update typescript-eslint monorepo to v5.56.0
    
    * fix(deps): update dependency @nestjs/axios to v0.1.1
    
    * fix(deps): update dependency fs-extra to v10.1.0
    
    * fix(deps): update dependency rxjs to v7.8.0
    
    * chore: work around TS2339
    
    * chore: revert 6a0140c8e7c061d0abf93cb52f9e891dbcc6860f
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- apps/generator-cli/src/app/services/version-manager.service.ts
+++ apps/generator-cli/src/app/services/version-manager.service.ts
@@ -217,7 +217,7 @@ export class VersionManagerService {
       this.logger.log(chalk.red('\nResponse:'));
       Object.entries(error.response.headers).forEach(a => this.logger.log(...a));
       this.logger.log();
-      error.response.data.on('data', data => this.logger.log(data.toString('utf8')));
+      (error.response.data as any).on('data', data => this.logger.log(data.toString('utf8')));
     }
   }
 
