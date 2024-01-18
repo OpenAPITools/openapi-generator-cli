5fe1ffa4790e6eeb6ffc47397e60c2367ff4f004
Elmar Beckmann <elmar+github@hey.com>
2024-01-18 03:29:36 +0100

    chore(deps): update dependencies and fixing vulnerabilities (#730)

--- apps/generator-cli/src/app/services/config.service.spec.ts
+++ apps/generator-cli/src/app/services/config.service.spec.ts
@@ -1,60 +1,54 @@
-import {Test} from '@nestjs/testing';
-import {ConfigService} from './config.service';
-import {LOGGER} from '../constants';
+import { Test } from '@nestjs/testing';
+import { ConfigService } from './config.service';
+import { LOGGER } from '../constants';
 
 jest.mock('fs-extra');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = jest.mocked(require('fs-extra'), true)
+const fs = jest.mocked(require('fs-extra'));
 
 describe('ConfigService', () => {
-
   let fixture: ConfigService;
 
-  const log = jest.fn()
+  const log = jest.fn();
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
-      providers: [
-        ConfigService,
-        {provide: LOGGER, useValue: {log}},
-      ],
+      providers: [ConfigService, { provide: LOGGER, useValue: { log } }],
     }).compile();
 
     fixture = moduleRef.get(ConfigService);
-    fs.writeJSONSync.mockReset()
-    fs.readJSONSync.mockReset()
-    fs.ensureFileSync.mockReset()
+    fs.writeJSONSync.mockReset();
+    fs.readJSONSync.mockReset();
+    fs.ensureFileSync.mockReset();
   });
 
   describe('API', () => {
-
     describe('get()', () => {
-
       describe.each([
         ['the config is undefined', undefined],
         ['the config empty', {}],
       ])('%s', (_, config) => {
-
         beforeEach(() => {
-          fs.readJSONSync.mockReturnValue(config)
-        })
+          fs.readJSONSync.mockReturnValue(config);
+        });
 
         it.each([
-          ['$schema', './node_modules/@openapitools/openapi-generator-cli/config.schema.json'],
+          [
+            '$schema',
+            './node_modules/@openapitools/openapi-generator-cli/config.schema.json',
+          ],
           ['spaces', 2],
-          ['generator-cli', {version: undefined}],
+          ['generator-cli', { version: undefined }],
         ])('the key "%s" returns %s as default', (key, expectedValue) => {
-          expect(fixture.get(key)).toEqual(expectedValue)
-        })
+          expect(fixture.get(key)).toEqual(expectedValue);
+        });
 
         it('can return a default value', () => {
-          expect(fixture.get('unknown', 'foo')).toEqual('foo')
-        })
-
-      })
+          expect(fixture.get('unknown', 'foo')).toEqual('foo');
+        });
+      });
 
       describe('the config has values', () => {
-
         beforeEach(() => {
           fs.readJSONSync.mockReturnValue({
             $schema: 'foo.json',
@@ -62,50 +56,52 @@ describe('ConfigService', () => {
             'generator-cli': {
               version: '1.2.3',
             },
-          })
-        })
+          });
+        });
 
         it('ensures the config file', () => {
-          fixture.get('some-path')
-          expect(fs.ensureFileSync).toHaveBeenNthCalledWith(1, fixture.configFile)
-        })
+          fixture.get('some-path');
+          expect(fs.ensureFileSync).toHaveBeenNthCalledWith(
+            1,
+            fixture.configFile
+          );
+        });
 
         it.each([
           ['$schema', 'foo.json'],
           ['spaces', 4],
-          ['generator-cli', {version: '1.2.3'}],
+          ['generator-cli', { version: '1.2.3' }],
           ['generator-cli.version', '1.2.3'],
         ])('"%s" returns %s as default', (key, expectedValue) => {
-          expect(fixture.get(key)).toEqual(expectedValue)
-          expect(fs.readJSONSync).toHaveBeenNthCalledWith(1, fixture.configFile, {throws: false, encoding: 'utf8'})
-        })
-
-      })
-
-    })
+          expect(fixture.get(key)).toEqual(expectedValue);
+          expect(fs.readJSONSync).toHaveBeenNthCalledWith(
+            1,
+            fixture.configFile,
+            { throws: false, encoding: 'utf8' }
+          );
+        });
+      });
+    });
 
     describe('has()', () => {
-
       beforeEach(() => {
         fs.readJSONSync.mockReturnValue({
           propFalsy: false,
           propUndefined: undefined,
           propNull: null,
-        })
-      })
+        });
+      });
 
       it('returns true, if the prop is set', () => {
-        expect(fixture.has('propFalsy')).toBeTruthy()
-        expect(fixture.has('propUndefined')).toBeTruthy()
-        expect(fixture.has('propNull')).toBeTruthy()
-      })
+        expect(fixture.has('propFalsy')).toBeTruthy();
+        expect(fixture.has('propUndefined')).toBeTruthy();
+        expect(fixture.has('propNull')).toBeTruthy();
+      });
 
       it('returns false, if the value is set', () => {
-        expect(fixture.has('foo')).toBeFalsy()
-      })
-
-
-    })
+        expect(fixture.has('foo')).toBeFalsy();
+      });
+    });
 
     describe('set()', () => {
       beforeEach(() => {
@@ -115,17 +111,21 @@ describe('ConfigService', () => {
           'generator-cli': {
             version: '1.2.3',
           },
-        })
+        });
 
-        fixture.set('generator-cli.version', '4.5.6')
-      })
+        fixture.set('generator-cli.version', '4.5.6');
+      });
 
       it('ensures the config file', () => {
-        expect(fs.ensureFileSync).toHaveBeenNthCalledWith(1, fixture.configFile)
-      })
+        expect(fs.ensureFileSync).toHaveBeenNthCalledWith(
+          1,
+          fixture.configFile
+        );
+      });
 
       it('saves the correct value', () => {
-        expect(fs.writeJSONSync).toHaveBeenNthCalledWith(1,
+        expect(fs.writeJSONSync).toHaveBeenNthCalledWith(
+          1,
           fixture.configFile,
           {
             $schema: 'foo.json',
@@ -133,15 +133,13 @@ describe('ConfigService', () => {
             'generator-cli': {
               version: '4.5.6',
             },
-          }, {
+          },
+          {
             encoding: 'utf8',
             spaces: 4,
           }
-        )
-      })
-
-    })
-
-  })
-
-})
+        );
+      });
+    });
+  });
+});
--- apps/generator-cli/webpack.config.js
+++ apps/generator-cli/webpack.config.js
@@ -1,37 +1,44 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { composePlugins, withNx } = require('@nx/webpack');
 const { name, version, ...packageConfig } = require('../../package.json');
 const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
 const { BannerPlugin } = require('webpack');
 const { omit } = require('lodash');
 
-module.exports = (config) => {
-  const basePackageValues = {
-    ...omit(packageConfig, ['scripts', 'dependencies', 'devDependencies']),
-    version,
-    name: `@${name}/openapi-generator-cli`,
-    description: 'A npm package wrapper for OpenAPI Generator (https://github.com/OpenAPITools/openapi-generator), generates which API client libraries (SDK generation), server stubs, documentation and configuration automatically given an OpenAPI Spec (v2, v3)',
-    scripts: {
-      postinstall: "opencollective || exit 0"
-    },
-    bin: {
-      'openapi-generator-cli': './main.js'
-    },
-    files: [
-      'config.schema.json',
-      'README.md',
-      'main.js'
-    ],
-    dependencies: {
-      'reflect-metadata': '',
-      '@nuxtjs/opencollective': ''
-    }
-  };
+// Nx plugins for webpack.
+module.exports = composePlugins(
+  withNx({
+    target: 'node',
+  }),
+  (config) => {
+    const basePackageValues = {
+      ...omit(packageConfig, ['scripts', 'dependencies', 'devDependencies']),
+      version,
+      name: `@${name}/openapi-generator-cli`,
+      description:
+        'A npm package wrapper for OpenAPI Generator (https://github.com/OpenAPITools/openapi-generator), generates which API client libraries (SDK generation), server stubs, documentation and configuration automatically given an OpenAPI Spec (v2, v3)',
+      scripts: {
+        postinstall: 'opencollective || exit 0',
+      },
+      bin: {
+        'openapi-generator-cli': './main.js',
+      },
+      files: ['config.schema.json', 'README.md', 'main.js'],
+      dependencies: {
+        'reflect-metadata': '',
+        '@nuxtjs/opencollective': '',
+        axios: '',
+      },
+    };
 
-  config.plugins.push(
-    new BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
-    new GeneratePackageJsonPlugin(basePackageValues, {
-      useInstalledVersions: true,
-    })
-  );
+    config.plugins.push(
+      new BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
+      new GeneratePackageJsonPlugin(basePackageValues, {
+        useInstalledVersions: true,
+      })
+    );
 
-  return config;
-};
+    return config;
+  }
+);
