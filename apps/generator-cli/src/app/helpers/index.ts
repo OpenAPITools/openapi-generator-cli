a5724ae5f556a53d56ba0c76775670f67c417fdf
mok-liee <6745620+mok-liee@users.noreply.github.com>
2024-03-01 00:13:34 +0100

    fix: add quotation marks only for windows paths

--- apps/generator-cli/src/app/helpers/index.ts
+++ apps/generator-cli/src/app/helpers/index.ts
@@ -1,7 +1,11 @@
+const isWin = () => process.platform === 'win32';
+
 /**
  * If JAVA_HOME is set, it returns `$JAVA_HOME/bin/java`
  * otherwise it returns `java` and it has to be in the `PATH`
  */
 export const javaCmd: string = process.env['JAVA_HOME']
-  ? `"${process.env['JAVA_HOME']}/bin/java"`
+  ? isWin()
+    ? `"${process.env['JAVA_HOME']}/bin/java"`
+    : `${process.env['JAVA_HOME']}/bin/java`
   : 'java';
