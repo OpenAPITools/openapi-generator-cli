7787f07edfe5a9d8d5272646bf54311a31c89e62
Jonathan <dev@jontze.com>
2024-04-16 08:53:04 +0200

    fix(app): configure http with proxy url if present in environment (#762)
    * chore(deps): install `https-proxy-agent` as dependency
    
    * fix(app): configure http agent with proxy url if present in environment
    
    Fixes #OpenAPITools/openapi-generator-cli#714

--- apps/generator-cli/src/app/app.module.ts
+++ apps/generator-cli/src/app/app.module.ts
@@ -1,16 +1,33 @@
-import {Inject, Module, OnApplicationBootstrap} from '@nestjs/common';
-import {HttpModule} from '@nestjs/axios';
-import {Command} from 'commander';
+import { Inject, Module, OnApplicationBootstrap } from '@nestjs/common';
+import { HttpModule, HttpModuleOptions } from '@nestjs/axios';
+import { Command } from 'commander';
 
-import {COMMANDER_PROGRAM, LOGGER} from './constants';
-import {VersionManagerController} from './controllers/version-manager.controller';
-import {ConfigService, GeneratorService, PassThroughService, UIService, VersionManagerService} from './services';
+import { COMMANDER_PROGRAM, LOGGER } from './constants';
+import { VersionManagerController } from './controllers/version-manager.controller';
+import {
+  ConfigService,
+  GeneratorService,
+  PassThroughService,
+  UIService,
+  VersionManagerService,
+} from './services';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+
+const proxyUrl = process.env.HTTP_PROXY || process.env.HTTPS_PROXY;
+const httpModuleConfig: HttpModuleOptions = {};
+
+if (proxyUrl) {
+  httpModuleConfig.proxy = false;
+  httpModuleConfig.httpsAgent = new HttpsProxyAgent(proxyUrl);
+}
 
 @Module({
-  imports: [HttpModule],
-  controllers: [
-    VersionManagerController
+  imports: [
+    HttpModule.register({
+      ...httpModuleConfig,
+    }),
   ],
+  controllers: [VersionManagerController],
   providers: [
     UIService,
     ConfigService,
@@ -19,26 +36,31 @@ import {ConfigService, GeneratorService, PassThroughService, UIService, VersionM
     VersionManagerService,
     {
       provide: COMMANDER_PROGRAM,
-      useValue: new Command('openapi-generator-cli').helpOption(false).usage('<command> [<args>]').option( '--openapitools <openapitools.json>', 'Use the specified openapi-generator-cli configuration file')
+      useValue: new Command('openapi-generator-cli')
+        .helpOption(false)
+        .usage('<command> [<args>]')
+        .option(
+          '--openapitools <openapitools.json>',
+          'Use the specified openapi-generator-cli configuration file',
+        ),
     },
-    {provide: LOGGER, useValue: console}
-  ]
+    { provide: LOGGER, useValue: console },
+  ],
 })
 export class AppModule implements OnApplicationBootstrap {
-
   constructor(
     @Inject(COMMANDER_PROGRAM) private readonly program: Command,
     private readonly versionManager: VersionManagerService,
-    private readonly passThroughService: PassThroughService
-  ) {
-  }
+    private readonly passThroughService: PassThroughService,
+  ) {}
 
   onApplicationBootstrap = async () => {
-
     let selectedVersion = this.versionManager.getSelectedVersion();
 
     if (!selectedVersion) {
-      const [{version}] = await this.versionManager.search(['latest']).toPromise();
+      const [{ version }] = await this.versionManager
+        .search(['latest'])
+        .toPromise();
       await this.versionManager.setSelectedVersion(version);
       selectedVersion = version;
     }
@@ -46,7 +68,5 @@ export class AppModule implements OnApplicationBootstrap {
     await this.versionManager.downloadIfNeeded(selectedVersion);
     await this.passThroughService.init();
     this.program.parse(process.argv);
-
   };
-
 }
