61d21114fb34accb222f7970c3b427d39468835f
Joscha Feth <joscha@feth.com>
2024-10-03 08:03:19 +0100

    chore: add nix flake (#826)
    * chore: add nix flake
    
    * chore: we do need npm
    
    ---------
    
    Co-authored-by: William Cheng <wing328hk@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "OpenAPI generator CLI nix flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";  
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            buildInputs = with pkgs;[
+              # These dependencies match the github workflows
+              yarn
+              nodejs_18
+            ];
+          };
+      }
+    );
+}
+
