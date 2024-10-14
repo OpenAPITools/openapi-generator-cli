9dc53a613cb2ca6de89cb55240b012b275ca0ded
GONZALEZ Adrian <pablo.gonzalez@laposte.fr>
2024-10-14 22:49:33 +0200

    feat: activate logging

--- apps/generator-cli/src/main.ts
+++ apps/generator-cli/src/main.ts
@@ -7,7 +7,9 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app/app.module';
 
 async function bootstrap() {
-  await NestFactory.createApplicationContext(AppModule, {logger: false});
+  await NestFactory.createApplicationContext(AppModule, {
+    logger: ['fatal', 'error', 'warn'],
+  });
 }
 
 bootstrap();
