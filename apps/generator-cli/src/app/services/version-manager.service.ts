5d619411caab03cb8809db5c6b0d7daf944b18e9
William Cheng <wing328hk@gmail.com>
2025-02-28 18:07:22 +0800

    feat(release): v7.12.0 release

--- apps/generator-cli/src/app/services/version-manager.service.ts
+++ apps/generator-cli/src/app/services/version-manager.service.ts
@@ -289,8 +289,15 @@ export class VersionManagerService {
 
   versions : Version[] = [
     {
+      version: '7.12.0',
+      versionTags: [ '7.12.0', 'stable', 'latest' ],
+      releaseDate: new Date("2025-02-28T06:24:58.285Z"),
+      installed: false,
+      downloadLink: 'https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.12.0/openapi-generator-cli-7.12.0.jar'
+    },
+    {
       version: '7.11.0',
-      versionTags: [ '7.11.0', 'stable', 'latest' ],
+      versionTags: [ '7.11.0', 'stable' ],
       releaseDate: new Date("2025-01-20T06:24:58.285Z"),
       installed: false,
       downloadLink: 'https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.11.0/openapi-generator-cli-7.11.0.jar'
