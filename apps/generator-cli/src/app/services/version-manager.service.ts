7f2dfa403c458b0488e42d1a0ac8398347f2830e
William Cheng <wing328hk@gmail.com>
2025-08-22 18:59:33 +0800

    chore(fix): fix tags

--- apps/generator-cli/src/app/services/version-manager.service.ts
+++ apps/generator-cli/src/app/services/version-manager.service.ts
@@ -289,7 +289,7 @@ export class VersionManagerService {
   versions : Version[] = [
     {
       version: '7.15.0',
-      versionTags: [ '7.14.0', 'stable', 'latest' ],
+      versionTags: [ '7.15.0', 'stable', 'latest' ],
       releaseDate: new Date("2025-08-22T06:24:58.285Z"),
       installed: false,
       downloadLink: 'https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.15.0/openapi-generator-cli-7.15.0.jar'
