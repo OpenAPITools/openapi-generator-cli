6c4ce0e98b15117c5f3c38b49b295dca79c4d227
William Cheng <wing328hk@gmail.com>
2025-09-15 14:48:10 +0800

    chore: fix ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,5 +2,9 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
   preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  transform: {
+    '^.+\\.(ts|html)$': 'ts-jest',
+  },
   projects: await getJestProjectsAsync(),
 });
