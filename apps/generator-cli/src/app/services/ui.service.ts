90a8db4da2a291df635914378ce9e462faf20fc8
ikeyan <kik@nvstl.com>
2026-02-23 12:05:23 +0900

    chore(deps): replace inquirer with `@inquirer/select` (#1136)
    * chore(deps): replace inquirer with `@inquirer/select`
    
    * chore(deps): downgrade @inquirer/select to ^1.3.3
    
    * chore: fix unit test version-manager.controller.spec.ts
    
    ---------
    
    Co-authored-by: William Cheng <wing328hk@gmail.com>

--- apps/generator-cli/src/app/services/ui.service.ts
+++ apps/generator-cli/src/app/services/ui.service.ts
@@ -1,62 +1,57 @@
 // import ora from 'ora'
-import {Injectable} from '@nestjs/common';
-import {prompt, Separator} from 'inquirer';
-import {getTable} from 'console.table'
+import { Injectable } from '@nestjs/common';
+import select, { Separator } from '@inquirer/select';
+import { getTable } from 'console.table';
+import { dim } from 'chalk';
 
 @Injectable()
 export class UIService {
-
   public async table<T>(config: {
-    name: string,
-    message: string,
-    printColNum?: boolean,
-    rows: Array<{ row: Record<string, unknown>, short: string, value: T }>,
+    message: string;
+    printColNum?: boolean;
+    rows: Array<{ row: Record<string, string>; short: string; value: T }>;
   }): Promise<T> {
+    const table: string = getTable(
+      config.rows.map(({ row }, index: number) => {
+        return config.printColNum === false ? row : { '#': index + 1, ...row };
+      }),
+    );
 
-
-    const table = getTable(config.rows.map(({row}, index: number) => {
-      return config.printColNum === false ? row : ({'#': index + 1, ...row});
-    }))
-
-    const [header, separator, ...rows] = table.trim().split('\n')
+    const [header, separator, ...rows] = table.trim().split('\n');
     return this.list({
-      name: config.name,
       message: config.message,
       choices: [
-        new Separator(header),
-        new Separator(separator),
-        ...rows.map((name: string, index: number) => ({
+        new Separator(dim(header)),
+        new Separator(dim(separator)),
+        ...rows.map((name, index) => ({
           name,
           short: config.rows[index].short,
           value: config.rows[index].value,
         })),
-        new Separator(separator),
-        new Separator(' '.repeat(separator.length)),
+        new Separator(dim(separator)),
+        new Separator(dim(' '.repeat(separator.length))),
       ],
-    })
+    });
   }
 
   public async list<T>(config: {
-    name: string,
-    message: string,
-    choices: Array<{ name: Record<string, unknown>, short?: string, value: T }>,
+    message: string;
+    choices: Array<{ name: string; short?: string; value: T } | Separator>;
   }): Promise<T> {
+    const pageSize = Math.max(1, process.stdout.rows - 2);
+    try {
+      const res = await select({
+        pageSize,
+        message: config.message,
+        choices: config.choices,
+      });
 
-    const separatorCount = config
-      .choices
-      .filter((c) => c instanceof Separator)
-      .length
-
-    const res = await prompt([{
-      type: 'list',
-      name: config.name,
-      pageSize: process.stdout.rows - separatorCount - 1,
-      message: config.message,
-      choices: config.choices,
-    }])
-
-    return res[config.name] as T
-
+      return res;
+    } catch (err) {
+      if (err instanceof Error && err.message.startsWith('User force closed the prompt with')) {
+        process.exit(0);
+      }
+      throw err;
+    }
   }
-
 }
