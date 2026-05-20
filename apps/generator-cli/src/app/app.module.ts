fa3786dd74c7b92ccd19b7a78242c3efe1568328
William Cheng <wing328hk@gmail.com>
2026-05-20 17:11:03 +0800

    chore(fix): fix proxy agent

--- apps/generator-cli/src/app/app.module.ts
+++ apps/generator-cli/src/app/app.module.ts
@@ -21,7 +21,6 @@ const proxyAgent = new ProxyAgent();
 if (hasHttpProxyEnvs) {
   httpModuleConfig.proxy = false;
   httpModuleConfig.httpAgent = proxyAgent;
-  console.log("Using HTTP proxy agent for HTTP requests");
 }
 
 if (hasHttpsProxyEnvs) {
