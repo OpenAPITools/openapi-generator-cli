df28723bab27f56f9e1f2336b82e2afc5b3c7957
William Cheng <wing328hk@gmail.com>
2026-05-20 17:23:30 +0800

    chore(fix): fix proxy agent

--- apps/generator-cli/src/proxy-agent.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'proxy-agent' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const ProxyAgent: new (...args: any[]) => any;
-  export default ProxyAgent;
-  export { ProxyAgent };
-}
