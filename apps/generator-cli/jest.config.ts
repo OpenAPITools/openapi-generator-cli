ec25c977f604874b447887f232dc051c56f29bdf
William Cheng <wing328hk@gmail.com>
2026-05-20 17:17:10 +0800

    chore(fix): fix proxy agent

--- apps/generator-cli/jest.config.ts
+++ apps/generator-cli/jest.config.ts
@@ -4,12 +4,9 @@ export default {
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[cm]?[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(proxy-agent|agent-base|http-proxy-agent|https-proxy-agent|pac-proxy-agent|socks-proxy-agent|proxy-from-env)/)',
-  ],
-  moduleFileExtensions: ['ts', 'js', 'mjs', 'cjs', 'html'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/generator-cli',
   // snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
