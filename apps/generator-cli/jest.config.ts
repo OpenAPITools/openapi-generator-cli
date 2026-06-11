c1464da47e2ea75a2aa85f414bf5e29295daebb9
William Cheng <wing328hk@gmail.com>
2026-06-11 16:33:49 +0800

    chore(deps): update concurrently to newer version

--- apps/generator-cli/jest.config.ts
+++ apps/generator-cli/jest.config.ts
@@ -7,7 +7,7 @@ export default {
     '^.+\\.[cm]?[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(glob|proxy-agent|agent-base|http-proxy-agent|https-proxy-agent|pac-proxy-agent|socks-proxy-agent|proxy-from-env)/)',
+    'node_modules/(?!(glob|minimatch|path-scurry|lru-cache|jackspeak|package-json-from-dist|foreground-child|cross-spawn|signal-exit|which|proxy-agent|agent-base|http-proxy-agent|https-proxy-agent|pac-proxy-agent|socks-proxy-agent|proxy-from-env)/)',
   ],
   moduleFileExtensions: ['ts', 'js', 'mjs', 'cjs', 'html'],
   coverageDirectory: '../../coverage/apps/generator-cli',
